e0a79b04c02029035effaf422a8f789e64caf99b
qizhg <qizhg@umich.edu>
2017-02-14 15:36:33 -0500

    add perceive, eGreedy

--- dqn/NeuralQLearner.lua
+++ dqn/NeuralQLearner.lua
@@ -44,6 +44,7 @@ function nql:__init(args)
 
     --Q-learning update frequency, update every update_freq steps, use minibatch_size transitions to do n_replay times weight gradient descent in each update 
     self.discount       = args.discount or 0.99 --discount factor, gamma
+    self.learn_start    = args.discount or 50000
     self.update_freq    = args.update_freq or 4
     self.minibatch_size = args.minibatch_size or 32
     self.n_replay       = args.n_replay or 1
@@ -53,19 +54,98 @@ function nql:__init(args)
     self.target_network = self.network:clone()
 
     --reward clipping
+    self.max_reward    = args.max_reward or 1
+    self.min_reward    = args.min_reward or -1
 
     --delta (Q error) clipping
 
     --Replay memory
 
     --current number of steps
-
-
-
-
-
-
-
-
-
+    self.step = 0
 end
+
+function nql:perceive(screen, reward, terminal)
+
+    --preproc screnn                                --screen (1,3,210,160)
+    state = self.preproc:forward(screen):clone()    --state  (1,84,84)
+    state = torch.reshape(state,self.state_dim)     --state  (84*84,)
+
+    --clip reward
+    if self.max_reward then
+        reward = math.min(reward,self.max_reward)
+    end
+
+    if self.min_reward then
+        reward = math.max(reward,self.min_reward)
+    end
+
+    --store (s = lastState, t = lastTerminal, a = lastAction, r=reward, s'=state)
+    if self.lastState then
+        self.replayMemory:add(self.lastState, self.lastTerminal, self.lastAction, reward)
+    end
+
+    --get fullstate, i.e. hist_len number of states stacked
+    self.replayMemory:add_recent_state(state,terminal)
+    local fullState = self.replayMemory:get_fullState()
+
+    --select and action, no action index (index = 0) when terminal
+    local actionIndex = 0
+    if not terminal then
+        actionIndex = self.eGreedy(fullState)
+    end
+    -----self.replayMemory:add_recent_action(actionIndex) --???? why do we need recent_action
+
+    --Q updates, after learn_start steps update every update_freq steps, use minibatch_size transitions to do n_replay times weight gradient descent in each update 
+    if self.step >= self.learn_start and self.step % self.update_freq==0 then
+        for i = 1, self.n_replay
+            self:qLearnMinibatch()
+        end
+    end
+
+    --target Q updates
+    if self.target_q and self.steps % self.target_q == 0 then
+        self.target_network = self.network:clone()
+    end 
+
+    self.lastState = state:clone()
+    self.lastTerminal = terminal
+    self.lastAction = actionIndex
+
+    self.step = self.step + 1
+
+    return actionIndex  
+end
+
+function nql:eGreedy(fullState)
+    self.ep =(self.ep_end +
+                math.max(0, (self.ep_start - self.ep_end) * (self.ep_endt -
+                math.max(0, self.numSteps - self.learn_start))/self.ep_endt))
+    if torch.uniform() < self.ep then
+        return torch.random(1, self.n_actions)
+    else
+        return self:greedy(fullState)
+    end
+    
+end
+
+function nql:Greedy(fullState)
+    --fullState {hist_len, 84, 84}
+
+    local q = self.network:forward(fullState):clone():float():squeeze() 
+           --squeeze removes all singleton dimensions of the tensor, s.t. q[a] is Q(s,a)
+    local qmax  = q[1]
+    local besta = {1}
+
+    for a =2, self.n_actions do
+        if q[a] > qmax then
+            qmax = q[a]
+            besta ={a}
+        elseif q[a] == qmax then
+            besta[#besta+1] =a
+        end
+    end
+
+    local r = torch.random(1,#besta)
+    return besta[r] --with random tie-breaking
+end 
--- dqn/train_agent.lua
+++ dqn/train_agent.lua
@@ -16,31 +16,18 @@ cmd:option('-env', '', 'name of environment to use')
 cmd:option('-framework', '', 'name of training framework')
 cmd:option('-game_path', '', 'path to environment file (ROM)')
 cmd:option('-env_params', '', 'string of environment parameters')
+cmd:option('-actrep', 4, 'epeat each action 4 times')
+cmd:option('-random_starts', 30, 'for every new episode, play null actions a random number of time [0,30]')
 
 cmd:option('-agent_name', '', 'name of the class defining the agent')
 cmd:option('-q_net', '', 'file defining the q net')
 cmd:option('-preproc_net', '', 'file defining the q net')
---cmd:option('-pool_frms', '','string of frame pooling parameters (e.g.: size=2,type="max")')
---cmd:option('-actrep', 1, 'how many times to repeat action')
---cmd:option('-random_starts', 0, 'play action 0 between 1 and random_starts ' ..'number of times at the start of each training episode')
 
---cmd:option('-name', '', 'filename used for saving network and training history')
---cmd:option('-network', '', 'reload pretrained network')
---cmd:option('-agent', '', 'name of agent file to use')
---cmd:option('-agent_params', '', 'string of agent parameters')
---cmd:option('-seed', 1, 'fixed input seed for repeatable experiments')
---cmd:option('-saveNetworkParams', false,'saves the agent network in a separate file')
---cmd:option('-prog_freq', 5*10^3, 'frequency of progress output')
---cmd:option('-save_freq', 5*10^4, 'the model is saved every save_freq steps')
---cmd:option('-eval_freq', 10^4, 'frequency of greedy evaluation')
---cmd:option('-save_versions', 0, '')
 
---cmd:option('-steps', 10^5, 'number of training steps to perform')
---cmd:option('-eval_steps', 10^5, 'number of evaluation steps')
+cmd:option('-steps',10^5, 'total steps to run')
 
---cmd:option('-verbose', 2,'the higher the level, the more information is printed to screen')
---cmd:option('-threads', 1, 'number of BLAS threads')
---cmd:option('-gpu', -1, 'gpu flag')
+
+
 
 cmd:text()
 
@@ -62,3 +49,22 @@ local game_actions = game_env:getActions()
 ---init NeuralQLearner
 args.actions = game_env:getActions()
 local agent = dqn[args.agent_name](args) --args.agent_name = "NeuralQLearner"
+
+--run steps
+local step = 0
+local screen, reward, terminal = game_env:getState()
+while step < args.steps do
+	step = step + 1
+	local action_index = agent:perceive(screen, reward, terminal)
+
+	if not terminal
+		screen, reward, terminal = game_env:step(game_actions[action_index],true) --true: training mode, losing a life = episode end = terminal 
+	else
+		if args.random_starts and args.random_starts>0 then
+			screen, reward, terminal = game_env:nextRandomGame()
+		else
+			screen, reward, terminal = game_env:newGame()
+		end
+	end
+end
+
